7942ff16533e31bd9f9c131872b9d368aa918865
♡ <53441218+saturnaliam@users.noreply.github.com>
2022-10-20 23:44:28 -0500

    Init shit

--- src/save-load.ts
+++ src/save-load.ts
@@ -1,13 +1,17 @@
-import { existsSync, writeFileSync, readFileSync } from 'node:fs';
+import { existsSync, writeFile, readFile } from 'node:fs';
+import { Buffer } from 'node:buffer';
+
 const SAVE_PATH = 'save/';
+const FILE_EXTENSION = '.lng';
 
 const objToJSON = (obj: object): string => {
     const keys = Object.keys(obj);
     let JSONOut = '{';
     for (let i = 0; i < keys.length; i++) {
-        JSONOut += `"${keys[i]}":"${obj[keys[i]]}",`;
+        JSONOut += `"${keys[i]}":"${obj[keys[i]]}"`;
+        if (!(i === keys.length - 1)) JSONOut += ',';
     }
-
+    
     JSONOut += '}';
     return JSONOut;
 }
@@ -15,19 +19,20 @@ const objToJSON = (obj: object): string => {
 const save = (input: object, inputFileName = 'language') => {
     let fileNum = 0;
     let fileName = inputFileName;
-    const languageJSON = objToJSON(input);
+    const languageJSON = new Uint8Array(Buffer.from(objToJSON(input)));
 
-    while (existsSync(SAVE_PATH + fileName)) {
+    while (existsSync(fileName)) {
         fileName = inputFileName + fileNum;
-        fileNum++
+        fileNum++;
     }
 
-    writeFileSync(SAVE_PATH + fileName, languageJSON);
+    writeFile(SAVE_PATH + fileName + FILE_EXTENSION, languageJSON, () => {});
 }
 
-const read = (inputFileName: string): object => {
-    let lang = readFileSync(SAVE_PATH + inputFileName);
-    return JSON.parse(lang);
+const read = (inputFileName: string) => {
+    readFile(SAVE_PATH + inputFileName + FILE_EXTENSION, (err, data) => {
+        console.log(JSON.parse(data.toString()));
+    })
 }
 
 export { read, save }
