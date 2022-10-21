0288be726862a03381708d1ed1f700297a736b9a
♡ <53441218+saturnaliam@users.noreply.github.com>
2022-10-21 17:41:18 -0500

    dsfa

--- src/save-load.ts
+++ src/save-load.ts
@@ -1,5 +1,6 @@
 import { existsSync, writeFile, readFile } from 'node:fs';
 import { Buffer } from 'node:buffer';
+import { FILE } from 'node:dns';
 
 const SAVE_PATH = 'save/';
 const FILE_EXTENSION = '.lng';
@@ -17,22 +18,33 @@ const objToJSON = (obj: object): string => {
 }
 
 const save = (input: object, inputFileName = 'language') => {
+    const data = {};
+    for (const [key, value] of Object.entries(input)) {
+        Object.defineProperty(data, key.replace(/_/g, ''), { 
+            value: value,
+            enumerable: true
+        });
+    }
+
     let fileNum = 0;
     let fileName = inputFileName;
-    const languageJSON = new Uint8Array(Buffer.from(objToJSON(input)));
-
-    while (existsSync(fileName)) {
+    const languageJSON = new Uint8Array(Buffer.from(objToJSON(data)));
+    console.log(objToJSON(data));
+    while (existsSync(fileName + FILE_EXTENSION)) {
         fileName = inputFileName + fileNum;
         fileNum++;
     }
-
-    writeFile(SAVE_PATH + fileName + FILE_EXTENSION, languageJSON, () => {});
+    writeFile(fileName + FILE_EXTENSION, languageJSON, () => {});
+    console.log(languageJSON);
 }
 
-const read = (inputFileName: string) => {
+const read = (inputFileName: string): object => {
+    console.log(SAVE_PATH + inputFileName + FILE_EXTENSION);
+    console.log(existsSync(SAVE_PATH + inputFileName + FILE_EXTENSION));
     readFile(SAVE_PATH + inputFileName + FILE_EXTENSION, (err, data) => {
-        console.log(JSON.parse(data.toString()));
+        return JSON.parse(data.toString());
     })
+    return {};
 }
 
 export { read, save }
